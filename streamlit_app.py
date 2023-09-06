98a9a38dc68d2f029e23395796401589f0cac57e
SethSnowflake <141778607+SethSnowflake@users.noreply.github.com>
2023-09-06 15:04:52 -0400

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
+import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -27,14 +29,14 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #Take the JSON response and normalize it.
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it to the screen as a table.
 streamlit.dataframe(fruityvice_normalized)
-
+# don't run anything past here while we troubleshoot
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -47,4 +49,4 @@ streamlit.dataframe(my_data_rows)
 #New Section to display fruityvice api response
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
-
+my_cur.execute("INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST VALUES ('from streamlit')");
